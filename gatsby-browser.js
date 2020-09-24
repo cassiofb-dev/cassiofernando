05b34fd78250e341895102e95718aaffac48750e
cassiofb-dev <cassiofb.souza@gmail.com>
2020-09-23 23:49:03 -0300

    fixed page reload

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,10 @@ const React = require('react');
 const Layout = require('./src/components/Layout').default;
 
 // Wraps every page in a component
-exports.wrapRootElement = ({ element, props }) => {
+exports.wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>
 }
+
+exports.replaceHydrateFunction = () => {
+  if(window.location.pathname !== '/') window.location.replace('/');
+};
