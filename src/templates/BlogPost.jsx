aa0530e68abaf96a308278a5fab66158fc59e63d
cassiofb-dev <cassiofb.souza@gmail.com>
2020-09-28 23:01:09 -0300

    helmet not working

--- src/templates/BlogPost.jsx
+++ src/templates/BlogPost.jsx
@@ -3,13 +3,10 @@ import { graphql } from 'gatsby';
 
 import './BlogPost.scss';
 
-import SEO from '../components/SEO';
-
 export default function BlogPost({ data: { markdownRemark: post} }) {
 
   return (
     <div className="blog-post">
-      <SEO title={post.frontmatter.title}/>
       <h2 className='title'>{post.frontmatter.title}</h2>
       <hr/>
       <div className='markdown' dangerouslySetInnerHTML={{ __html: post.html }}></div>
