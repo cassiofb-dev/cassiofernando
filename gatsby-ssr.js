80880075e0ee8f4abd91543f70ba9e2c6af5b50e
cassiofb-dev <cassiofb.souza@gmail.com>
2020-09-28 23:11:45 -0300

    added ssr

---
+++ gatsby-ssr.js
@@ -0,0 +1,11 @@
+const React = require('react');
+const Layout = require('./src/components/Layout').default;
+
+// Wraps every page in a component
+exports.wrapPageElement = ({ element }) => {
+  return (
+    <Layout>
+      {element}
+    </Layout>
+  );
+}
