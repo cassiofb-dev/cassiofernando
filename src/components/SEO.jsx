af855fb702e4acaddca054536e57b4e8f83ea1a0
cassiofb-dev <cassiofb.souza@gmail.com>
2020-10-05 22:07:36 -0300

    fixed shortcut type

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
 
-import favicon from '../assets/images/logo_short.png.png';
+import favicon from '../assets/images/logo_short.png';
 
 export default function SEO({ description, language, metaImage, title, pathname }) {
   const { site } = useStaticQuery(
